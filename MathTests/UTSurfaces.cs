5e07098ca6e20dda10996ed5c15949e4902b5963
Ninthchain <73429297+Ninthchain@users.noreply.github.com>
2023-03-10 17:46:21 +0300

    Done

--- MathTests/UTSurfaces.cs
+++ MathTests/UTSurfaces.cs
@@ -14,14 +14,14 @@ public class UnitTest1
     [Fact]
     public void IsCorrect_CountCircleSurface()
     {
-        _testOutputHelper.WriteLine("Testing CountCircleSurfaces(double)");
+        _testOutputHelper.WriteLine("Testing CountCircleSurfaces(double)\n");
         Assert.Equal(79, Double.Round(Surface.CountCircleSurface(5.0)));
     }
 
     [Fact]
     public void IsCorrect_CountTriangleSurface()
     {
-        _testOutputHelper.WriteLine("Testing CountTriangleSurface(double)");
+        _testOutputHelper.WriteLine("Testing CountTriangleSurface(double)\n");
         Assert.Equal(11, Double.Round(Surface.CountTriangleSurface(5.0, 5.0, 5.0)));
     }
 }
--- TestTask.Tests/Usings.cs
+++
@@ -1,2 +0,0 @@
-
-lobal using Xunit;
