e4abb29bcb0d1095f1af5faea3278129e4f373e1
Ninthchain <73429297+Ninthchain@users.noreply.github.com>
2023-03-10 15:21:42 +0300

    Base task completed

--- TestTask/MyMath/Surface.cs
+++ TestTask/MyMath/Surface.cs
@@ -4,6 +4,14 @@ namespace TestTask.MyMath
 {
     public static class Surface
     {
-        public static double Circle(double radius) => PI * Pow(radius, 2);
+        public static double CountCircleSurface(double radius) => PI * Pow(radius, 2);
+
+        public static double CountTriangleSurface(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            
+            //Heron's formula used
+            return Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
     }
 }
