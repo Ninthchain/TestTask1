614c165242381aa1977196b3eef0c84bd7527043
Ninthchain <73429297+Ninthchain@users.noreply.github.com>
2023-03-10 15:17:35 +0300

    first commit

---
+++ TestTask/MyMath/Surface.cs
@@ -0,0 +1,9 @@
+using static System.Math;
+
+namespace TestTask.MyMath
+{
+    public static class Surface
+    {
+        public static double Circle(double radius) => PI * Pow(radius, 2);
+    }
+}
