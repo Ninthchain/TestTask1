3e98c89cca0ef6eff59f16eaa4ead82eba357c4d
Ninthchain <73429297+Ninthchain@users.noreply.github.com>
2023-03-12 17:33:59 +0300

    Added task #4

---
+++ TaskFour/query.sql
@@ -0,0 +1,40 @@
+CREATE TABLE Products (
+                          Id INT PRIMARY KEY,
+                          "Name" TEXT
+);
+
+INSERT INTO Products
+VALUES
+    (1, 'C# in a Nutshell'),
+    (2, 'KIA Rio'),
+    (3, 'ASUS Rog Strix');
+
+CREATE TABLE Categories (
+                            Id INT PRIMARY KEY,
+                            "Name" TEXT
+);
+
+INSERT INTO Categories
+VALUES
+    (1, 'Book'),
+    (2, 'Car'),
+    (3, 'Gift');
+
+CREATE TABLE ProductCategories (
+    ProductId INT FOREIGN KEY REFERENCES Products(Id),
+    CategoryId INT FOREIGN KEY REFERENCES Categories(Id),
+    PRIMARY KEY (ProductId, CategoryId)
+);
+
+INSERT INTO ProductCategories
+VALUES
+    (1, 1),
+    (1, 3),
+    (2, 2);
+
+SELECT P."Name", C."Name"
+FROM Products P
+         LEFT JOIN ProductCategories PC
+                   ON P.Id = PC.ProductId
+         LEFT JOIN Categories C
+                   ON PC.CategoryId = C.Id;
